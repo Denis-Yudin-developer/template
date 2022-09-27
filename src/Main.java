283ec933a141eded6e1d62e3ea07a62349582407
denis.yudin <rng48322@gmail.com>
2022-09-27 03:40:10 +0300

    test commit pr

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fuck you");
+        System.out.println("And you to");
         System.out.println("Hello world!");
     }
 }
